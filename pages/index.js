9e784ff686134e193ebf6a9ea89a784c44e010fc
nikolayzz <nikolay.erendjenov@gmail.com>
2023-08-19 02:16:55 +0300

    remove getstatic

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ import MainHome from '@/components/Main/MainHome';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
 
-export const getStaticProps = async () => {
-  const url = process.env.NEXT_PUBLIC_API_HOST;
-  const email = process.env.EMAIL;
-  const test = 'test';
+// export const getStaticProps = async () => {
+//   const url = process.env.NEXT_PUBLIC_API_HOST;
+//   const email = process.env.EMAIL;
+//   const test = 'test';
 
-  return {
-    props: { url },
-  };
-};
+//   return {
+//     props: { url },
+//   };
+// };
 
 const Home = (props) => {
   return (
