a81116275140d8f0b798af1aab38e987a5ec2631
nikolayzz <nikolay.erendjenov@gmail.com>
2023-08-19 18:29:39 +0300

    env

--- pages/api/consult.js
+++ pages/api/consult.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 export default (req, res) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    service: "gmail",
     port: 465,
     secure: true,
     logger: true,
     debug: true,
     secureConnection: false,
     auth: {
-      user: 'bankrot.cntr@gmail.com',
-      pass: 'cjwogzyizeetduuu',
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
     tls: {
       rejectUnauthorized: true,
@@ -20,8 +20,8 @@ export default (req, res) => {
   });
 
   const message = {
-    to: 'bankrot.cntr@gmail.com',
-    subject: 'Запись на консультацию',
+    to: "bankrot.cntr@gmail.com",
+    subject: "Запись на консультацию",
     text: `Запись на консультацию от
         Имя: ${req.body.data.name}, Телефон: ${req.body.data.phone}
         Тест: ${process.env.API_HOST}`,
@@ -30,10 +30,10 @@ export default (req, res) => {
   transporter.sendMail(message, (err, data) => {
     if (err) {
       console.log(err);
-      res.send('error' + JSON.stringify(err));
+      res.send("error" + JSON.stringify(err));
     } else {
-      console.log('mail send', process.env.PASSWORD);
-      res.send('success');
+      console.log("mail send", process.env.PASSWORD);
+      res.send("success");
     }
   });
 };
--- service/mailService.js
+++ service/mailService.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  service: "gmail",
   port: 465,
   secure: true,
   logger: true,
   debug: true,
   secureConnection: false,
   auth: {
-    user: 'bankrot.cntr@gmail.com',
-    pass: 'cjwogzyizeetduuu',
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
   tls: {
     rejectUnauthorized: true,
@@ -23,7 +23,7 @@ const sendEmail = (message) => {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email has been sent successfully', process.env.API_HOST);
+      console.log("Email has been sent successfully", process.env.API_HOST);
     }
     transporter.close();
   });
