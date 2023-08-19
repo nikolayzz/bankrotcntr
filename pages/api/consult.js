6f7f690310eadc11d506d397e618a8a9ea863a66
nikolayzz <nikolay.erendjenov@gmail.com>
2023-08-19 19:03:38 +0300

    cons

--- pages/api/consult.js
+++ pages/api/consult.js
@@ -1,6 +1,24 @@
-import sendEmail from "@/service/mailService";
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
-export default async function handler(req, res) {
+export default (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    service: "gmail",
+    port: 465,
+    secure: true,
+    logger: true,
+    debug: true,
+    secureConnection: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: true,
+    },
+  });
+
   const message = {
     to: "bankrot.cntr@gmail.com",
     subject: `Запись на консультацию. Клиент ${req.body.data.name}`,
@@ -8,7 +26,13 @@ export default async function handler(req, res) {
     Имя: ${req.body.data.name}, Телефон: ${req.body.data.phone}`,
   };
 
-  sendEmail(message);
-  console.log(message);
-  res.send("Thank you");
-}
+  transporter.sendMail(message, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log("mail send", process.env.PASSWORD);
+      res.send("success");
+    }
+  });
+};
